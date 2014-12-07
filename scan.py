ca2476cc7124816627625541378dc80a4662188f
Mark Spicer <mds4680@rit.edu>
2014-12-07 17:38:48 -0500

    Added module to scan IP addresses

---
+++ scan.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# Name: scan.py
+# Author: Mark Spicer
+# Purpose: To scan a provided subnet for responsive machines
+
+# Import the necessary modules.
+import os
+import nmap
+import threading
+
+# Create a scan class that creates a thread for each host
+class scan (threading.Thread):
+    # Create the object.
+	def __init__(self, threadID, name, ip):
+		threading.Thread.__init__(self)
+		self.threadID = threadID
+		self.name = name
+		self.ip = ip
+		self.info = 0
+
+    # Run the port scanner on the IP address.
+	def run(self):
+		nm = nmap.PortScanner()
+		nm.scan(hosts=self.ip, arguments='-sP')
+		if nm[self.ip].state() == 'up':
+			nm.scan(hosts=self.ip, arguments='-T5')
+
+		self.info = nm
+
+	# Return the info for the IP address.
+	def results(self):
+		return self.info
