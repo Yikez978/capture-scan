aff3741ca7ca597b55306f8b64e3108fb29069ab
Mark Spicer <mds4680@rit.edu>
2014-12-07 15:46:46 -0500

    Added a section to import a packet capture csv

--- capture-scan.py
+++ capture-scan.py
@@ -3,9 +3,9 @@
 # Author: Mark Spicer
 # Purpose: Reads a packet capture and then port scans each unique host.
 
-# Necessary modules.
+# Import necessary modules.
 import argparse         # Used to parse command line arguements.
-import csv
+import csv              # Used to parse csv files.
 
 # Parse command line arguements passed to the script.
 parser = argparse.ArgumentParser(description=
@@ -13,3 +13,9 @@ parser = argparse.ArgumentParser(description=
 parser.add_argument('-i, --input', dest='input_file',
     help='Capture file to parse.')
 args = parser.parse_args()
+
+# Open captured input file.
+with open(args.input_file, 'rb') as csvfile:
+    capture = csv.DictReader(csvfile)
+    for row in capture:
+        print(row['Source'],row['Destination'])
